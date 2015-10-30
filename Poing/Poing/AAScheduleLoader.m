1a9e9f320e86b547a9bf7eb9f7bba5a305f2ef1f
Korry Luke <koluke@iolani.org>
2015-10-30 13:46:57 +0900

    Rewrite verifyAppVersion
    Modify to work with App Store and in-house versions, prevent
    unnecessary update prompts

--- Poing/Poing/AAScheduleLoader.m
+++ Poing/Poing/AAScheduleLoader.m
@@ -102,18 +102,36 @@
 
 + (void)verifyCurrentAppVersion {
     NSString *currentVer = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
+    NSString *bundleID = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleIdentifier"];
     [PFConfig getConfigInBackgroundWithBlock:^(PFConfig *config, NSError *error) {
-        if(!error && ![config[@"currentTFVer"] isEqual:nil])  {
-            NSString *appVer = config[@"currentTFVer"];
-            NSString *message = config[@"messageTF"];
-            NSString *header = config[@"messageHeaderTF"];
-            NSLog(@"Fetched latest version info %@", appVer);
-            if(![currentVer isEqualToString:appVer]) {
-                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:header message:message delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
-                NSLog(@"App is out of date.  Version %@ reported, currently running version %@.  Received prompt: %@, %@", appVer, currentVer, header, message);
-                [alert show];
+        if(!error) {
+            if([bundleID isEqualToString:config[@"appleDistroBundleID"]]) {
+                NSString *currentTFVer = config[@"currentTFVer"];
+                NSString *currentASVer = config[@"currentASVer"];
+                NSString *currentARVer = config[@"currentARVer"];
+                NSLog(@"Fetched latest version info for Poing Apple distro v%@, v%@, and v%@.", currentASVer, currentTFVer, currentARVer);
+                if(!error && ![currentVer isEqualToString:currentTFVer] && ![currentVer isEqualToString:currentASVer] && ![currentVer isEqualToString:currentARVer]) {
+                    NSString *message = config[@"messageAppleDistro"];
+                    NSString *header = config[@"headerAppleDistro"];
+                    NSLog(@"Poing v%@ is out of date.  Currently valid versions are: %@, %@, %@.  Received prompt: %@, %@", currentVer, currentASVer, currentTFVer, currentARVer, header, message);
+                    
+                    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:header message:message delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+                    [alert show];
+                }
+            } else if([bundleID isEqualToString:config[@"selfServiceBundleID"]]) {
+                NSString *appVer = config[@"currentSSVer"];
+                NSString *message = config[@"messageSS"];
+                NSString *header = config[@"messageHeaderSS"];
+                NSLog(@"Fetched latest version info for Poing in-house v%@.", appVer);
+                if(![currentVer isEqualToString:appVer]) {
+                    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:header message:message delegate:self cancelButtonTitle:nil otherButtonTitles:nil, nil];
+                    NSLog(@"App is out of date.  Version %@ reported, currently running version %@.  Received prompt: %@, %@", appVer, currentVer, header, message);
+                    [alert show];
+                } else {
+                    NSLog(@"App install is up to date.");
+                }
             } else {
-                NSLog(@"App install is up to date.");
+                NSLog(@"Currently running unsupported bundle ID: %@.", bundleID);
             }
         }
         else {
